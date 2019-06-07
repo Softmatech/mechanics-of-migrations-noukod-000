76bd58110fc5ecbb434e40f1f558f1e7c045fa00
Andy Feidje Joseph <ingandy75@gmail.com>
2019-06-07 15:17:33 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -4,4 +4,10 @@ class CreateArtists < ActiveRecord::Migration
 
   def down
   end
+
+  ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
+
 end
